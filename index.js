ac214f0672ed413781c18b54bef2d9cfef0b273f
Zak Stamps <theoneandonlyzako@gmail.com>
2021-05-18 20:35:36 -0700

    pushing new code for safety

--- index.js
+++ index.js
@@ -2,23 +2,12 @@ const inquirer = require('inquirer');
 const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
-
-// const profileDataArgs = process.argv.slice(2);
-
-// const [name, github] = profileDataArgs;
-  
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-// if (err) throw new Error(err);
-
-// console.log('portfolio complete! checkout index.html to see the output!');
-// });
 const promptUser = () => {
  return inquirer.prompt([
     {
         type: 'input',
         name: 'name',
-        message: 'What is your Name? (required)',
+        message: 'Enter company name (required)',
         validate: nameInput => {
             if (nameInput) {
                 return true;
@@ -28,56 +17,6 @@ const promptUser = () => {
             }
         }
     },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your Github username? (required)',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('please enter github!');
-                return false;                
-            }
-        }
-    },
-    {
-        type: 'confirm',
-        name: 'confirmAbout',
-        message: 'Would you like to enter some information about yourself for an "About" section?',
-        default: true
-      },
-      {
-        type: 'input',
-        name: 'about',
-        message: 'Provide some information about yourself:',
-        when: ({ confirmAbout }) => {
-          if (confirmAbout) {
-            return true;
-          } else {
-            return false;
-          }
-        }
-      },
-    {
-        type: 'input',
-        name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('please enter your GitHub link');
-                return false;                
-            }
-        }
-      },
-      {
-        type: 'confirm',
-        name: 'confirmAbout',
-        message: 'Would you like to enter some information about yourself for an "About" section?',
-        default: true
-      }
 ]);
 };
 
@@ -88,25 +27,59 @@ const promptProject = portfolioData => {
     }
     console.log(`
   =================
-  Add a New Project
+  Add a New Employee or Intern
   =================
   `);
     return inquirer.prompt([
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'Enter employee or intern name'
       },
       {
         type: 'input',
-        name: 'description',
-        message: 'Provide a description of the project (Required)'
-      },
+        name: 'id',
+        message: 'Enter your employee ID (required)',
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log('please enter your employee ID!');
+                return false;                
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address (required)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('please enter your email address');
+                return false;                
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'phone',
+        message: 'Enter your office phone number (required)',
+        validate: phoneInput => {
+            if (phoneInput) {
+                return true;
+            } else {
+                console.log('please enter your email address');
+                return false;                
+            }
+        }
+    },
       {
         type: 'checkbox',
         name: 'languages',
-        message: 'What did you build this project with? (Check all that apply)',
-        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        message: 'What did your position? (Check all that apply)',
+        choices: ['Manager', 'Engineer', 'Intern']
       },
       {
         type: 'input',
@@ -120,9 +93,22 @@ const promptProject = portfolioData => {
         default: false
       },
       {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username? (required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('please enter github!');
+                return false;                
+            }
+        }
+    },
+      {
         type: 'confirm',
         name: 'confirmAddProject',
-        message: 'Would you like to enter another project?',
+        message: 'Would you like to enter another team member?',
         default: false
       }
     ])
--- src/page-template.js
+++ src/page-template.js
@@ -27,8 +27,7 @@ const generateProjects = projectsArr => {
               Built With:
               ${languages.join(', ')}
             </h5>
-            <p>${description}</p>
-            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            
           </div>
         `;
         })
@@ -78,11 +77,6 @@ module.exports = templateData => {
     <header>
       <div class="container flex-row justify-space-between align-center py-3">
         <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
-        <nav class="flex-row">
-          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-            header.github
-          }">GitHub</a>
-        </nav>
       </div>
     </header>
     <main class="container my-5">
